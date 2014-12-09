62828b0aea0eb41243086765402df3477f8cf4f3
lilott8 <hidden>
2014-12-09 11:13:09 -0800

    Fixed some lee2bit bugs

--- Source/lee2bit.cc
+++ Source/lee2bit.cc
@@ -26,8 +26,6 @@ void Lee2Bit::start(Route r) {
     claim("Using 2bit!", kWarning);
 
     if(is_valid()) {
-        kWaveFrontSource.empty();
-        kTraceBack.empty();
         kWaveFrontSource.push_front(kSource);
 
         Path* p = new Path();
@@ -69,7 +67,7 @@ int Lee2Bit::solve_recursive(int iteration) {
     // Base case 2: We found the sink
     if (is_sink(curr)) {
         // add the sink to the trace_back
-        kTraceBack.push_back(curr);
+        kTraceBackSource.push_back(curr);
         kMap->get_map()->at(curr.get_x()).at(curr.get_y())->set_type(LeeNode::NodeType::TRACEBACK);
         claim("We found the sink: " + curr.to_string(), kDebug);
         return iteration;
@@ -93,17 +91,17 @@ int Lee2Bit::solve_recursive(int iteration) {
     solve_recursive(iteration + 1);
 
     // Handle the trace_back generation for the algorithm [change]
-    if (kTraceBack.size() > 0) {
-        if (kTraceBack.back().get_cost() == 1) {
+    if (kTraceBackSource.size() > 0) {
+        if (kTraceBackSource.back().get_cost() == 1) {
             // Handle the 3->1 hand off
-            if (curr.get_cost() == 3 && is_adjacent(curr, kTraceBack.back())) {
-                kTraceBack.push_back(curr);
+            if (curr.get_cost() == 3 && is_adjacent(curr, kTraceBackSource.back())) {
+                kTraceBackSource.push_back(curr);
                 kMap->get_map()->at(curr.get_x()).at(curr.get_y())->set_type(LeeNode::NodeType::TRACEBACK);
             }
             // Otherwise just decrement as you should
         } else {
-            if (curr.get_cost() < kTraceBack.back().get_cost() && is_adjacent(curr, kTraceBack.back())) {
-                kTraceBack.push_back(curr);
+            if (curr.get_cost() < kTraceBackSource.back().get_cost() && is_adjacent(curr, kTraceBackSource.back())) {
+                kTraceBackSource.push_back(curr);
                 kMap->get_map()->at(curr.get_x()).at(curr.get_y())->set_type(LeeNode::NodeType::TRACEBACK);
             }
         }
@@ -166,7 +164,7 @@ vector<LeeNode> Lee2Bit::get_adjacent_coordinates(LeeNode c, int iteration) {
     return results;
 }
 
-LeeNode Lee2Bit::calculate_metric(LeeNode a, int iteration,) {
+LeeNode Lee2Bit::calculate_metric(LeeNode a, int iteration) {
     LeeNode temp = a;
 
     int dist = calculate_lees_distance(temp);
